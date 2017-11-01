100dfb0d1a8d7b22043113c9369f004ae90db62d
KevTLW <student@3-15.78x660.nycboe.org>
2017-11-01 16:42:13 -0400

    Fix hw get

--- Team8200/src/main/java/org/firstinspires/ftc/team8200/Hardware.java
+++ Team8200/src/main/java/org/firstinspires/ftc/team8200/Hardware.java
@@ -30,6 +30,10 @@ public class Hardware {
         leftMotor.setPower(0);
         rightMotor.setPower(0);
 
+        // Change to RUN_USING_ENCODERS if encoders are installed.
+        leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
         // Set all motors to run without encoders unless they're being used
         leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
